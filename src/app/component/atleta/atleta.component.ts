7c6236e74be6eba2fdcb9e18dc48704c266fe4e2
Daniel <daniel.h.nunes@aluno.senai.br>
2026-08-14 14:36:03 -0300

    fazendo o cadastro do atleta funcionar

--- src/app/component/atleta/atleta.component.ts
+++ src/app/component/atleta/atleta.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-atleta',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './atleta.component.html',
   styleUrl: './atleta.component.css'
 })
-export class AtletaComponent {
 
+export class AtletaComponent {
+  nome = '';
+  cpf = 0;
+  sexo = '';
+  cep = 0;
+  ruaLogradouro = '';
+  bairro = '';
+  cidade = '';
+  uf = '';
+
+  exibirDados(){
+    console.log(this.nome, this.cpf, this.sexo, this.cep, this.ruaLogradouro, this.bairro, this.cidade, this.uf)
+
+    this.LimparDados();
+  }
+
+  LimparDados(){
+    this.nome = '';
+    this.cpf = 0;
+    this.sexo = '';
+    this.cep = 0;
+    this.ruaLogradouro = '';
+    this.bairro = '';
+    this.cidade = '';
+    this.uf = '';
+  }
+  
 }
