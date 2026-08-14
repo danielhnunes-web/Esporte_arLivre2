d6cb0797cffc9360a96c10d7a37672c22c778b77
Daniel <daniel.h.nunes@aluno.senai.br>
2026-08-14 16:45:24 -0300

    criei arquivo corrida e fiz estilização da pag corrida

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 
 import { HomeComponent } from './component/home/home.component';
 import { AtletaComponent } from './component/atleta/atleta.component';
+import { CorridaComponent } from './component/corrida/corrida.component';
 
 export const routes: Routes = [
 
     { path: "", redirectTo: "/home", pathMatch: "full"},
 
     { path: "home", component: HomeComponent},
-    { path: "cadastroAtleta", component: AtletaComponent}
+    { path: "cadastroAtleta", component: AtletaComponent},
+    { path: "cadastroCorrida", component: CorridaComponent},
 ];
