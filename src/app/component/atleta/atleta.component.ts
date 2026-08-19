fe6184e8588a682eead02ab2b7a14cc5554ae0ac
Daniel <daniel.h.nunes@aluno.senai.br>
2026-08-19 15:44:08 -0300

    aprendendo como fazer para alterar os dados da pag

--- src/app/component/atleta/atleta.component.ts
+++ src/app/component/atleta/atleta.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 import { AtletaServiceService } from '../../service/atleta-service.service';
 import { Atleta } from '../../models/Atleta';
@@ -13,16 +14,22 @@ import { Atleta } from '../../models/Atleta';
 })
 
 export class AtletaComponent {
-  nome = '';
-  cpf = 0;
-  sexo = '';
-  cep = 0;
-  ruaLogradouro = '';
-  bairro = '';
-  cidade = '';
-  uf = '';
+  nome = ''
+  cpf = 0
+  sexo = ''
+  cep = 0
+  ruaLogradouro = ''
+  bairro = ''
+  cidade = ''
+  uf = ''
 
-  constructor(private atletaService: AtletaServiceService) {}
+  idAtleta = 0
+  editar = false
+
+  constructor(
+    private atletaService: AtletaServiceService,
+    private http: ActivatedRoute,
+    ) {}
 
   exibirDados(){
     console.log(this.nome, this.cpf, this.sexo, this.cep, this.ruaLogradouro, this.bairro, this.cidade, this.uf)
@@ -30,43 +37,89 @@ export class AtletaComponent {
     this.LimparDados();
   };
 
+  ngOnInit(){
+    this.idAtleta = Number(this.http.snapshot.paramMap.get('id'))
+    
+    if(this.idAtleta > 0){
+      this.editar = true
+      this.carregaDados(this.idAtleta)
+    }
+  };
+
   LimparDados(){
-    this.nome = '';
-    this.cpf = 0;
-    this.sexo = '';
-    this.cep = 0;
-    this.ruaLogradouro = '';
-    this.bairro = '';
-    this.cidade = '';
-    this.uf = '';
+    this.nome = ''
+    this.cpf = 0
+    this.sexo = ''
+    this.cep = 0
+    this.ruaLogradouro = ''
+    this.bairro = ''
+    this.cidade = ''
+    this.uf = ''
+  };
+
+  carregaDados(idAtleta: number){
+    this.atletaService.listarAtleta(idAtleta)
+    .subscribe({
+      next:(dadosAtleta)=> {
+        this.nome = dadosAtleta.nome
+        this.cpf = dadosAtleta.cpf
+        this.sexo = dadosAtleta.sexo
+        this.cep = dadosAtleta.cep
+        this.ruaLogradouro = dadosAtleta.ruaLogradouro
+        this.bairro = dadosAtleta.bairro
+        this.cidade = dadosAtleta.cidade
+        this.uf = dadosAtleta.uf
+
+
+      },
+      error:(msgErro)=> {
+        console.log('ERRO AO LISTAR ATLETA', msgErro)
+      }
+    })
   };
 
   enviarDadosAtleta(){
     const atleta = new Atleta();
-    atleta.nome = this.nome;
-    atleta.cpf = this.cpf;
-    atleta.sexo = this.sexo;
-    atleta.cep = this.cep;
-    atleta.ruaLogradouro = this.ruaLogradouro;
-    atleta.bairro = this.bairro;
-    atleta.cidade = this.cidade;
-    atleta.uf = this.uf;
+    atleta.nome = this.nome
+    atleta.cpf = this.cpf
+    atleta.sexo = this.sexo
+    atleta.cep = this.cep
+    atleta.ruaLogradouro = this.ruaLogradouro
+    atleta.bairro = this.bairro
+    atleta.cidade = this.cidade
+    atleta.uf = this.uf
 
-    this.atletaService.salvarAtleta(atleta)
-    .subscribe({
-      next: (resposta) => {
-        console.log(resposta)
-      },
-      error: (msgErro) => {
-        console.log(msgErro)
-      }
-    });
+    if(this.editar){
+      atleta.id = this.idAtleta
+      
+      this.atletaService.alterarAtleta(atleta)
+      .subscribe({
+        next: (resposta) => {
+          console.log(resposta)
+        },
+        error: (msgErro) => {
+          console.log(msgErro)
+        }
+      })
+    }else{
+      this.atletaService.salvarAtleta(atleta)
+      .subscribe({
+        next: (resposta) => {
+          console.log(resposta)
+        },
+        error: (msgErro) => {
+          console.log(msgErro)
+        }
+      });
+
+    }
+
 
     this.LimparDados();
 
     this.atletaService.listarAtletas();
 
     
-  }
+  };
   
 }
